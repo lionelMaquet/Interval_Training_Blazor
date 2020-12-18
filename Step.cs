9ea2218b901718ed4c328d156ecda8fd052091d5
Lionel Maquet <lionel.maquet@gmail.com>
2020-12-18 18:52:53 +0100

    Data entry ui and logic done

---
+++ Step.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Interval_Training
+{
+    public class Step
+    {
+        public int CurrentMinutes;
+        public int CurrentSpeed;
+
+        public Step()
+        {
+            CurrentMinutes = 0;
+            CurrentSpeed = 1;
+        }
+    }
+}
---
+++ StepRep.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Interval_Training
+{
+    public class StepRep
+    {
+        public int reps = 1;
+    }
+}
